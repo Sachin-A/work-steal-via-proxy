8082b6713b3aa52f360f578f00d6abbd003c210c
Sachin Ashok <sachinashok.g@gmail.com>
2022-12-04 16:36:49 -0600

    Handle empty request from wrk2

--- src/proxy.py
+++ src/proxy.py
@@ -6,10 +6,16 @@ import time
 #********* CONSTANT VARIABLES *********
 BACKLOG = 50            # how many pending connections queue will hold
 MAX_DATA_RECV = 4096    # max number of bytes we receive at once
-DEBUG = True            # set to True to see the debug msgs
+DEBUG = 1               # set to True to see the debug msgs
+
+def printCustom(*args):
+  global DEBUG
+  if DEBUG:
+      print("Custom print: " + " ".join(map(str, args)))
 
 def handle_request(q, queueLock):
-  
+
+  x = 1
   while(1):
     flag = False
     queueLock.acquire()
@@ -34,53 +40,74 @@ def handle_request(q, queueLock):
       webserver = ""
       port = 3000
 
-      if (DEBUG):
-        print(request)
-        #print(first_line)
-        #print("URL:", url)
+      printCustom("Request: ", request)
+      #printCustom(first_line)
+      #printCustom("URL:", url)
 
-      print("Connect to:", webserver, port)
+      if request.decode() == "":
+          printCustom("Empty request, closing.")
+          conn.close()
+          continue
+
+      printCustom("Connect to:", webserver, port)
 
       try:
         # create a socket to connect to the web server
+        printCustom("Debug: A")
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        printCustom("Debug: B")
         s.connect((webserver, port))
+        printCustom("Debug: C")
         s.send(request)         # send request to webserver
+        printCustom("Debug: D")
 
         while 1:
           # receive data from web server
+          #printCustom("Debug: E")
           data = s.recv(MAX_DATA_RECV)
-
+          #printCustom("Debug: F")
           if (len(data) > 0):
             # send to browser
             conn.send(data)
+            #printCustom("Debug: G")
           else:
+            #printCustom("Debug: H")
             break
+
+        #printCustom("Debug: I")
         s.close()
+        #printCustom("Request handler: Closed socket to webserver")
         conn.close()
+        #printCustom("Request handler: Closed connection from client to proxy")
+        print("Processed: ", x)
+        x = x + 1
 
       except socket.error as e:
         if s:
           s.close()
+          printCustom("Request handler error: Closed socket to webserver")
         if conn:
           conn.close()
-        print("Could not open socket")
+          printCustom("Request handler error: Closed connection from client to proxy")
+        printCustom("Could not open socket")
         sys.exit(1)
 
 #********* MAIN PROGRAM ***************
 def main():
 
   # check the length of command running
-  if (len(sys.argv) < 2):
-    print("usage: proxy <port>")
+  if (len(sys.argv) < 3):
+    printCustom("usage: proxy <port> <debug-bool>")
     return sys.stdout
     #exit(1)
 
   # host and port info.
   host = ""               # blank for localhost
   port = int(sys.argv[1]) # port from argument
+  global DEBUG
+  DEBUG = int(sys.argv[2])
 
-  print(port)
+  printCustom("Serving on port:", port)
 
   try:
     # create a socket
@@ -93,9 +120,10 @@ def main():
     s.listen(BACKLOG)
 
   except socket.error as e:
-    if e.errno != errno.EINTR:
+    if s:
       s.close()
-    print("Could not open socket")
+      printCustom("Main thread error: Closed socket from client to proxy")
+    printCustom("Main thread error: Could not open socket")
     sys.exit(1)
 
   q = queue.Queue()
@@ -104,15 +132,19 @@ def main():
   thread.start()
 
   # get the connection from client
+  x = 1
   while 1:
     conn, client_addr = s.accept()
-
+    print(type(conn), type(client_addr))
+    print("Connection no:", x)
     queueLock.acquire()
-    q.put((conn, client_addr))
+    q.put(conn, client_addr)
+    printCustom("Queue size:", q.qsize())
     queueLock.release()
+    x = x + 1
 
-    #handle_request(conn, client_addr)
   s.close()
+  printCustom("Main thread: Closed socket from client to proxy")
   thread.join()
 
 if __name__ == '__main__':
--- src/server.py
+++ src/server.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 # encoding: utf-8
 import json
+import time
 from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def index():
+    time.sleep(1)
     return json.dumps({'name': 'alice',
                        'email': 'alice@outlook.com'})
 if __name__ == '__main__':
